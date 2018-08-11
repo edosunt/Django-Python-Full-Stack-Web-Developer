08f59b607ed203600a6f8482cf0c90b76ec0b412
EdoSunt <edo@motoyuk.com>
2018-08-11 15:29:19 +1000

    Python Function

---
+++ PYTHON/Game.py
@@ -0,0 +1,47 @@
+import random
+
+gameOn = "" #Global variable to know whether the game is over
+
+def code_gen():
+    """
+    This function is to generate the 3 digit code. None should be the same
+    """
+    angka = [str(num) for num in range(10)]
+    random.shuffle(angka)
+    return (angka[:3])
+
+def check_code(key,guess):
+    """
+    This function is to receive a number and check the code.
+    Give hints of "Close" if you guess the number correct but position is wrong
+    "match" if the number is correct and position is correct
+    "Nope" if the number and position is incorrect
+    """
+    if guess == key:
+        return "You cracked the code"
+
+    clue=[]
+
+    for i in [0,1,2]:
+        if guess[i] == key [i]:
+            clue.append("Match")
+        elif guess[i] in key:
+            clue.append("Close")
+
+    if clue == []:
+        return "Nope"
+    else:
+        return clue
+
+#Main
+code = code_gen()
+print(code)
+
+while gameOn != "You cracked the code":
+    try:
+        guess = list(input("What is your guess? "))
+        gameOn = check_code(code,guess)
+        for clue in gameOn:
+            print(clue)
+    except:
+        print("Error occured - wrong input, try again, CTRL+Z to exit")
---
+++ PYTHON/test.py
@@ -0,0 +1,6 @@
+a="abcdef"
+b="abg"
+
+print(a[0:4])
+print(len(a))
+print(a[len(b):])
